a21ff8949ebe7d8826ba7ba0d4d1d6669837beb1
Truong Dao <truong.dao.do@gmail.com>
2020-06-11 17:44:46 -0400

    Solution_Node3_TD

--- server.js
+++ server.js
@@ -5,11 +5,44 @@ const morgan = require('morgan');
 
 const { users } = require('./data/users');
 
+const user = {
+  _id: '1007',
+  name: 'Maisy',
+  friends: ['1006', '1008', '1009'],
+  avatarUrl: '/images/profile-pics/000002.jpg',
+};
+
 let currentUser = {};
 
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
+};
+
+const handleHomepage = (req, res) => {
+  res.status(200).render('pages/homepage', { users: users });
+};
+
+const handleProfilePage = (req, res) => {
+  res.status(200).render('pages/profile', {
+    users: users,
+    user: user,
+  });
+};
+
+const handleSignin = (req, res) => {
+  res.status(200).render('pages/signin');
+};
+
+const handleName = (req, res) => {
+  let firstName = req.query.firstName;
+  console.log(firstName);
+  let loginName = users.find((user) => user.name === firstName);
+  if (loginName !== undefined) {
+    res.status(200).redirect('/users/1007');
+  } else {
+    res.status(404).render('pages/signin');
+  }
 };
 
 // -----------------------------------------------------
@@ -21,6 +54,10 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
+  .get('/', handleHomepage)
+  .get('/users/1007', handleProfilePage)
+  .get('/signin', handleSignin)
+  .get('/getname', handleName)
 
   // a catchall endpoint that will send the 404 message.
   .get('*', handleFourOhFour)
